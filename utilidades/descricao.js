8ca05d9424b2a3ccfa51e5f62ea55ef2d3d14ae8
Pedroh7-alt <pedro.h.sousa7@ba.estudante.senai.br>
2026-09-09 08:53:20 -0300

    Update na descrição

--- utilidades/descricao.js
+++ utilidades/descricao.js
@@ -1,3 +1,3 @@
-const descricaoPokeDex = "A PokéDex é uma ferramenta de busca para analisar as estatísticas do pokémon desejado, são elas: altura, peso, tipo, habilidades e movimentos";
+const descricaoPokeDex = "A PokéDex é uma ferramenta de busca para analisar as estatísticas e características de um pokémon desejado, são elas: altura, peso, tipo, habilidades, movimentos e as formas variantes do pokémon";
 
 module.exports = descricaoPokeDex;
